cbd4561d39b8795d8fe419269916a66fc8822a47
Zakharevich <butthead4z@mail.ru>
2018-12-30 16:13:53 +0300

    added singleton

---
+++ c#/Patterns.Singleton/SingletonClass.cs
@@ -0,0 +1,16 @@
+﻿namespace Patterns.Singleton
+{
+    public class SingletonClass
+    {
+        private static SingletonClass _instance;
+
+        private SingletonClass()
+        {}
+
+        public static SingletonClass GetInstance()
+        {
+            _instance = _instance ?? new SingletonClass();
+            return _instance;
+        }
+    }
+}
---
+++ c#/Patterns.Tests/SingletonTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using Patterns.Singleton;
+
+namespace Patterns.Tests
+{
+    [TestFixture]
+    public class SingletonTests
+    {
+        [Test]
+        public void ShouldBeOnlyOneInstance()
+        {
+            var instance1 = SingletonClass.GetInstance();
+            var instance2 = SingletonClass.GetInstance();
+       
+            Assert.True(object.ReferenceEquals(instance1, instance2));
+        }
+    }
+}
